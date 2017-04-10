ddbd84cc79594589d0c9388a9be5c67986be832b
jwmcgavin <Jordan@jordans-mbp.fc.ca>
2017-04-10 11:01:29 -0400

    AJAX and JSON started

---
+++ admin/ajaxQuery.php
@@ -0,0 +1,15 @@
+<?php
+	include 'phpscripts/connect.php';
+
+	$link->set_charset("utf8");
+
+	// set some post stuff up here
+	$vidNumber = $_GET["gallery_id"];	
+
+	$videoQuery = "SELECT * FROM tbl_movies WHERE gallery_id = '$vidNumber'";
+	$result = mysqli_query($link, $videoQuery);
+	$row = mysqli_fetch_assoc($result);
+
+	echo json_encode($row);
+	//echo mysqli_num_rows($result);		
+?>
